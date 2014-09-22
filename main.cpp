875eb6117419be38c1e1773a95f44c03461f273c
Karsten Ahnert <karsten.ahnert@gmx.de>
2014-09-22 08:16:29 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ * Date: 2014-09-22
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ * Copyright: Karsten Ahnert
+ */
+
+
+#include "map_iterator.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+
+int main( int argc , char *argv[] )
+{
+    
+    return 0;
+}
